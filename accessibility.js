cf6817f66ef71a5a31e58f464de52e0a6daf50f5
Niyo <ngumus14@gmail.com>
2024-02-15 17:46:35 +0100

    Schriftgrößen-Einstellung in localStorage speichern

--- accessibility.js
+++ accessibility.js
@@ -1,21 +1,29 @@
-document.querySelectorAll('.font-size-up').forEach(item => {
-    item.addEventListener('click', function () {
-        const root = document.documentElement;
-        const currentSize = parseInt(window.getComputedStyle(root).getPropertyValue('--main-font-size'));
+document.addEventListener('DOMContentLoaded', function () {
+    const root = document.documentElement;
+    const savedFontSize = localStorage.getItem('fontSize');
+    if (savedFontSize) {
+        root.style.setProperty('--main-font-size', savedFontSize);
+    }
 
-        if (currentSize < 18) {
-            root.style.setProperty('--main-font-size', `${currentSize + 1}px`);
-        }
+    document.querySelectorAll('.font-size-up').forEach(item => {
+        item.addEventListener('click', function () {
+            const currentSize = parseInt(window.getComputedStyle(root).getPropertyValue('--main-font-size'));
+            if (currentSize < 18) {
+                const newSize = `${currentSize + 1}px`;
+                root.style.setProperty('--main-font-size', newSize);
+                localStorage.setItem('fontSize', newSize);
+            }
+        });
     });
-});
 
-document.querySelectorAll('.font-size-down').forEach(item => {
-    item.addEventListener('click', function () {
-        const root = document.documentElement;
-        const currentSize = parseInt(window.getComputedStyle(root).getPropertyValue('--main-font-size'));
-
-        if (currentSize > 14) {
-            root.style.setProperty('--main-font-size', `${currentSize - 1}px`);
-        }
+    document.querySelectorAll('.font-size-down').forEach(item => {
+        item.addEventListener('click', function () {
+            const currentSize = parseInt(window.getComputedStyle(root).getPropertyValue('--main-font-size'));
+            if (currentSize > 14) {
+                const newSize = `${currentSize - 1}px`;
+                root.style.setProperty('--main-font-size', newSize);
+                localStorage.setItem('fontSize', newSize);
+            }
+        });
     });
 });
