9893a9b4a618bbf6e14cf4d8ae3e15436a3b5f11
Niyo <ngumus14@gmail.com>
2024-02-19 15:16:19 +0100

    Bootstrap-Dateien hinzugefügt

--- template/navbar.php
+++ template/navbar.php
@@ -5,7 +5,6 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title><?php echo $pageTitle; ?></title>
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
   <link rel="preconnect" href="https://fonts.googleapis.com">
@@ -13,6 +12,7 @@
   <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
   <link rel="icon" type="image/png" sizes="16x16" href="/StuttgartWebRedesign/img/favicon-16x16.png">
   <link rel="apple-touch-icon" sizes="180x180" href="/StuttgartWebRedesign/img/apple-touch-icon.png">
+  <link rel="stylesheet" href="/StuttgartWebRedesign/bootstrap/custom.css">
   <link rel="stylesheet" href="/StuttgartWebRedesign/styles/style.css">
 </head>
 
