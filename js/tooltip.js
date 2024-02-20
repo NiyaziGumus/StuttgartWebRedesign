21177924097a6f5e29e786ca68707306ccd51d24
Niyo <ngumus14@gmail.com>
2024-02-20 14:34:21 +0100

    Stuttgart Thema hinzugefügt

--- js/tooltip.js
+++ js/tooltip.js
@@ -1,23 +1,29 @@
 (() => {
-  'use strict'
+  'use strict';
 
   const getStoredTheme = () => localStorage.getItem('theme');
   const setStoredTheme = theme => localStorage.setItem('theme', theme);
 
   const setTheme = theme => {
-    document.documentElement.removeAttribute('data-bs-theme'); 
-    document.documentElement.setAttribute('data-bs-theme', theme); 
+    document.documentElement.removeAttribute('data-bs-theme');
+    document.documentElement.setAttribute('data-bs-theme', theme);
   };
 
   const initTheme = () => {
-    const storedTheme = getStoredTheme() || 'light'; 
+    const storedTheme = getStoredTheme() || 'light';
     setTheme(storedTheme);
     document.querySelectorAll(`[data-bs-theme-value="${storedTheme}"]`).forEach(btn => btn.classList.add('active'));
   };
 
-  window.addEventListener('DOMContentLoaded', () => {
-    initTheme();
+  var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
+  var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl);
+  });
 
+  window.addEventListener('DOMContentLoaded', () => {
+    initTheme(); 
+
+ 
     document.querySelectorAll('[data-bs-theme-value]').forEach(toggle => {
       toggle.addEventListener('click', (e) => {
         const theme = toggle.getAttribute('data-bs-theme-value');
@@ -25,6 +31,8 @@
         setTheme(theme);
         document.querySelectorAll('[data-bs-theme-value]').forEach(btn => btn.classList.remove('active'));
         toggle.classList.add('active');
+
+        tooltipList.forEach(tooltip => tooltip.hide());
       });
     });
   });
